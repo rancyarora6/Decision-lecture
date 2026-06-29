b81531ddc20af90f3d1c4d6c0f78ee577b849189
rancyarora6 <rancyarora6@gmail.com>
2026-06-29 13:22:55 +0530

    mrp

---
+++ discount.py
@@ -0,0 +1,9 @@
+mrp=float(input("enter mrp of book :"))
+dis = 0
+net = 0
+if mrp >= 500:
+   dis = mrp * 10/100
+else:
+    dis = mrp * 5/100
+    net = mrp - dis
+    print(f"MRP={mrp},Dis={dis},NET={net}")
