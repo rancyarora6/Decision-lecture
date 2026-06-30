42850b23c3d5492f84afab5807ae53248c3de246
rancyarora6 <rancyarora6@gmail.com>
2026-06-30 17:36:39 +0530

    sum

---
+++ sum.py
@@ -0,0 +1,7 @@
+# variable - it is a name which is give to memory location of ram where we can store data
+# variable is a data storage tool 
+'''sum of two numbers'''
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+Total=n1+n2 
+print(f"The sum is {Total}") 
