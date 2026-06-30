b941472946db74b5c95c164c4989e2e47ba50484
rancyarora6 <rancyarora6@gmail.com>
2026-06-30 19:14:54 +0530

    Week

---
+++ Week.py
@@ -0,0 +1,18 @@
+# Enter day of the week and name of the day
+Day=int(input("Enter day of week b/w 1-7"))
+if Day==1:
+    print("Monday")
+elif Day==2:
+    print("Tuesday")
+elif Day==3:  
+    print("Wednesday")
+elif Day==4:
+    print("Thursday")
+elif Day==5:
+    print("Friday")
+elif Day==6:
+    print("Saturday")
+elif Day==7:
+    print("Sunday")
+else:
+    print("enter valid week day")                
