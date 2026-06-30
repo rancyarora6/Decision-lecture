f65ed8ac26d8f66ce9f6400fe2f3914c093abf72
rancyarora6 <rancyarora6@gmail.com>
2026-06-30 18:27:54 +0530

    product

---
+++ product.py
@@ -0,0 +1,7 @@
+'''Production of three numbers'''
+n1=int(input("Enter first number:"))
+n2=int(input("Enter second number:"))
+n3=int(input("Enter third number:"))
+total=n1*n2*n3
+print(f"The product of {n1} and {n2} and {n3} is {total}")
+print("f{n1}*{n2}*{n3}={total}")
